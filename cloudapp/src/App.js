7901e116b6cd0938d168794817a8e32ed58a394b
QiuFilms <qiufilms@gmail.com>
2022-05-23 20:56:22 +0200

    com

--- cloudapp/src/App.js
+++ cloudapp/src/App.js
@@ -5,15 +5,18 @@ import Menu from './Components/Menu';
 import { Routes, Route, Link } from "react-router-dom";
 import LoginForm from './Components/subComponents/LoginForm';
 import RegisterForm from './Components/subComponents/RegisterForm';
+import AuthProvider from './Components/Context/AuthContext';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Menu/>} />
-      <Route path="home" element={<Main />} />
-      <Route path="login" element={<LoginForm />} />
-      <Route path="register" element={<RegisterForm />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Menu/>} />
+        <Route path="home" element={<Main />} />
+        <Route path="login" element={<LoginForm />} />
+        <Route path="register" element={<RegisterForm />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
--- cloudapp/src/Components/subComponents/RegisterForm.js
+++ cloudapp/src/Components/subComponents/RegisterForm.js
@@ -1,27 +1,56 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import Header from './Header'
+import { useAuth } from '../Context/AuthContext'
 
 const RegisterForm = () => {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+  const { signUp } = useAuth()
+  const [error, setError] = useState()
+  const [loading, setLoading] = useState(false)
+
+  async function handleSumbit(e){
+    e.preventDefault()
+
+    if(passwordRef.current.value !== passwordConfirmRef.current.value){
+      return setError("Passwords do not match")
+    }
+
+    try {
+      setError("")
+      setLoading(true)
+      await signUp(emailRef.current.value, passwordRef.current.value)
+    } catch (error) {
+      setError("Failed to create an account")
+    }
+
+    setLoading(false)
+  }
+
   return (
     <>
         <Header/>
         <div className="w-25 p-3 m-auto mt-5">
         <h3>Register</h3>
         <hr/>
-        <div className="mb-3">
-            <label for="exampleFormControlInput1" className="form-label">Username</label>
-            <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Username"/>
-        </div>
-        <div className="mb-3">
-            <label for="exampleFormControlInput1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email"/>
-        </div>
-        <div className="mb-3">
-            <label for="exampleFormControlInput1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleFormControlInput1" placeholder="Password"/>
-        </div>
-        <button type="button" class="btn btn-info float-end">Register</button>
-        </div>
+        {error && <p className="text-danger">{error}</p>}
+        <form onSubmit={handleSumbit}>
+          <div className="mb-3">
+              <label className="form-label">Email address</label>
+              <input type="email" className="form-control"placeholder="Email address" required ref={emailRef}/>
+          </div>
+          <div className="mb-3">
+              <label className="form-label">Passoword</label>
+              <input type="password" className="form-control"  placeholder="Passoword" required ref={passwordRef}/>
+          </div>
+          <div className="mb-3">
+              <label className="form-label">Confirm password</label>
+              <input type="password" className="form-control" placeholder="Confirm password" required ref={passwordConfirmRef}/>
+          </div>
+          <button type="button" class="btn btn-info float-end" disabled={loading}>Register</button>
+          </form>
+          </div>
     </>
   )
 }
